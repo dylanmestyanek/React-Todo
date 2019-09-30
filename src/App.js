d8cec87f9faf5d64839fe651b4efb1b2a0585507
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-30 13:44:35 -0500

    App.js is now stateful with todos, rendering TodoList container. TodoList is now mapping over todo state and rendering a Todo component for each todo object.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from 'react'
+
+import TodoList from "./components/TodoComponents/TodoList"
+import TodoForm from "./components/TodoComponents/TodoForm"
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state={
+      todos: []
+    }
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Todo extends React.Component {
+    render(){
+        return (
+            <div>{this.props.todo}</div>
+        );
+    }
+}
+
+export default Todo
