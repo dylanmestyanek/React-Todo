ee7673f0e51aa9d81faeb5d1f3b09f96b5a98da3
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-30 16:34:45 -0500

    MVP complete. Styling of entire application complete.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,8 +1,10 @@
 import React from "react"
 
+import styled from "styled-components"
+
 class TodoForm extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
             todo: ''
         }
@@ -20,19 +22,60 @@ class TodoForm extends React.Component {
 
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
+            <Form onSubmit={this.handleSubmit}>
                 <input 
                     type="text"
                     value={this.state.todo}
                     name="todo"
-                    placeholder="Add Todo Task"
+                    placeholder="+ New Task"
                     onChange={this.handleChange}
                 />
-                <button>Add Task</button>
-                <button>Remove Selected</button>
-            </form>
+                <ButtonContainer>
+                    <button type="submit">Add Task</button>
+                    <button onClick={this.props.removeTasks}>Remove Selected</button>
+                </ButtonContainer>
+            </Form>
         )
     }
 }
 
 export default TodoForm
+
+const Form = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    input {
+        width: 80%;
+        padding: 5px 0 5px 20px;
+        margin-bottom: 20px;
+        border: none;
+        border-radius: 5px;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    button {
+        width: 150px;
+        background: #8ACEF0;
+        padding: 5px 0;
+        border: none;
+        border-radius: 5px;
+        font-size: 1.4rem;
+        transition: all .3s;
+        font-family: 'Roboto', sans-serif;
+
+        &:hover {
+            background: #064B6E;
+            color: white;
+        }
+    }
+`;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,29 @@
 import React from "react"
 import Todo from "./Todo"
 
+import styled from "styled-components"
+
 class TodoList extends React.Component {
     render(){
-        return this.props.todos.map(todo => <Todo todo={todo} toggleCompleted={this.props.toggleCompleted} />);
+        return (
+            <TodoContainer>
+                {
+                    this.props.todos.map(todo => <Todo todo={todo} toggleCompleted={this.props.toggleCompleted} />)
+                    
+                }
+            </TodoContainer>
+        )
     }
 }
 
 export default TodoList;
+
+const TodoContainer = styled.div`
+    background: white;
+    width: 80%;
+    margin-top: 20px;
+    padding: 5px 20px;
+    font-size: 1.5rem;
+    line-height: 2;
+    border-radius: 5px;
+`;
